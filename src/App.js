0d732a7cc5eae0fc7fa015465358ea3778e4a07e
maarcusf <mengo123>
2020-09-10 10:10:17 -0300

    Pegando o resultado em JSON da API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      nutri: []
+    };
+    
+  }
+
+  componentDidMount(){
+    let url = 'https://sujeitoprogramador.com/rn-api/?api=posts';
+    fetch(url)
+    .then((r)=>r.json())
+    .then((json)=>{
+      let state = this.state;
+      state.nutri = json;
+      this.setState(state);
+      console.log(json)
+    })
+  }
+
+  render(){
+    return(
+      <div>
+
+      </div>
+    )
+  }
+  
 }
 
 export default App;
